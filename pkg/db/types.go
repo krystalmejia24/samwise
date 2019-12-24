ff07b48f19bc1d8ce834c2522daf8fe80431387a
krystalmejia24 <krystal.mejia@cbsinteractive.com>
2019-12-24 13:06:10 -0500

    small cleanup

--- pkg/db/types.go
+++ pkg/db/types.go
@@ -1,16 +1,19 @@
 package db
 
+//Encoder struct represents encoding instance persisted in the repo
 type Encoder struct {
-	IP     string  `json:"ip"`
-	Config *Config `json:"config,omitempty"`
-	Stream *Stream `json:"event,omitempty"`
+	IP     string    `json:"ip"`
+	Config *Config   `json:"config,omitempty"`
+	Stream *[]Stream `json:"event,omitempty"`
 }
 
+//Config struct holds authentication needed for encoding
 type Config struct {
 	Username string `json:"username,omitempty"`
 	APIKey   string `json:"api_key,omitempty"`
 }
 
+//Stream is the event associated to an encoding instance
 type Stream struct {
 	ID int `json:"id,omitempty"`
 }
