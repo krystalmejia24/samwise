3c707a19fe2a60ce362547050d6acb99ad045e94
krystalmejia24 <krystal.mejia@cbsinteractive.com>
2019-12-26 19:14:12 -0500

    adding tests. adding makefile

---
+++ Makefile
@@ -0,0 +1,21 @@
+name ?= samwise
+cmd = main.go
+
+withColors = sed ''/PASS/s//$$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$$(printf "\033[31mFAIL\033[0m")/''
+
+run:
+	go run $(cmd)
+
+build:
+	go build $(cmd)
+
+test:
+	go test -v -race ./... | $(withColors)
+
+cover:
+	go test -cover ./... -covermode=atomic
+
+test_cover:
+	go test -v ./... -tags test -race -coverprofile=coverage.txt -covermode=atomic | $(withColors)
+
+.PHONY: run build test cover test_cover
---
+++ pkg/db/encoder_test.go
@@ -0,0 +1,120 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func defaultEncoder() *Encoder {
+	return &Encoder{
+		IP: "10.10.10.10",
+		Config: &Config{
+			Username: "username",
+			APIKey:   "api-key",
+		},
+		Stream: &[]Stream{
+			Stream{ID: 1},
+			Stream{ID: 2},
+		},
+	}
+}
+
+func TestRedis_CreateEncoder(t *testing.T) {
+	tests := []struct {
+		name    string
+		e       *Encoder
+		wantErr bool
+	}{
+		{
+			name:    "when encoder is created, it is properly stored in redis and no error is thrown",
+			e:       defaultEncoder(),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := NewRepo()
+			if err := r.CreateEncoder(tt.e); (err != nil) != tt.wantErr {
+				t.Errorf("Redis.CreateEncoder() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestRedis_GetEncoder(t *testing.T) {
+	expectedEncoder := defaultEncoder()
+	wantEnc, err := json.Marshal(expectedEncoder)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tests := []struct {
+		name    string
+		ip      string
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			name:    "get encoder is properly returned in redis and no error is thrown",
+			ip:      expectedEncoder.IP,
+			want:    string(wantEnc),
+			wantErr: false,
+		},
+		{
+			name:    "get encoder properly returns error if encoder not found",
+			ip:      "some ip",
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := NewRepo()
+			got, err := r.GetEncoder(tt.ip)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Redis.GetEncoder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Redis.GetEncoder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRepo_DeleteEncoder(t *testing.T) {
+	tests := []struct {
+		name    string
+		ip      string
+		want    int64
+		wantErr bool
+	}{
+		{
+			name:    "delete encoder is properly returned in redis and no error is thrown",
+			ip:      defaultEncoder().IP,
+			want:    1,
+			wantErr: false,
+		},
+		{
+			name:    "get encoder properly returns error if encoder not found",
+			ip:      "some ip",
+			want:    0,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := NewRepo()
+			got, err := r.DeleteEncoder(tt.ip)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Repo.DeleteEncoder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Repo.DeleteEncoder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
